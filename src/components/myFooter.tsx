3a6f9c6707114c9ec5ca1035474b503ad05c1214
Janik Teege <janikteege@pm.me>
2022-08-13 01:08:49 +0200

    link fix

--- src/components/myFooter.tsx
+++ src/components/myFooter.tsx
@@ -5,7 +5,7 @@ export const MyFooter = () => {
   return (
     <div className="App">
       <a rel="stylesheet" href="https://dsgvo.calcowave.com" >Privacy Policy</a>
-      <a rel="stylesheet" href="https://imprint.calcowave.com" >Imprint</a>
+      <a rel="stylesheet" href="https://imp.calcowave.com" >Imprint</a>
     </div>
   );
 }
